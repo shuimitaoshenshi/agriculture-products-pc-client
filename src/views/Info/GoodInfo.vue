526f0554a0cefbd11fe0f93006de88f2f5555af3
zxd <1281696826@qq.com>
2022-11-16 09:30:11 +0800

    creat info branch

--- src/views/Info/GoodInfo.vue
+++ src/views/Info/GoodInfo.vue
@@ -2,36 +2,44 @@
   <div class="goodinfo-container">
     <Head></Head>
     <div class="goodinfo-main">
-      <div class="left">
-        <div class="carousel">
-          <Carousel :max-width="500"></Carousel>
+      <div class="showgood">
+        <div class="left">
+          <div class="carousel">
+            <Carousel :max-width="500"></Carousel>
+          </div>
+        </div>
+        <div class="right">
+          <div class="name">
+            <h1>{{ good.name }}</h1>
+          </div>
+          <div class="price" ref="priceRef">
+            ¥{{ good.price[this.currentSize] }}
+          </div>
+          <!--  选择商品规格/数量 -->
+          <div class="sizebt">
+            <h3>选择商品规格/数量</h3>
+            <v-col cols="auto">
+              <v-btn-toggle v-model="currentSize" color="deep-purple ">
+                <v-btn
+                  focus
+                  v-for="{ id, size } in sizes"
+                  :key="id"
+                  @click="changeSize(size)"
+                  >{{ size }}
+                </v-btn>
+              </v-btn-toggle>
+            </v-col>
+          </div>
+          <!--  选择商品规格/数量 -->
         </div>
       </div>
-      <div class="right">
-        <div class="name">
-          <h1>{{ good.name }}</h1>
-        </div>
-        <div class="price" ref="priceRef">
-          ¥{{ good.price[this.currentSize] }}
-        </div>
-        <!--  选择商品规格/数量 -->
-        <div class="sizebt">
-          <v-col cols="auto">
-            <v-btn-toggle v-model="currentSize" color="deep-purple ">
-              <v-btn
-                focus
-                v-for="{ id, size } in sizes"
-                :key="id"
-                @click="changeSize(size)"
-                >{{ size }}
-              </v-btn>
-            </v-btn-toggle>
-          </v-col>
-        </div>
-        <!--  选择商品规格/数量 -->
-      </div>
-
       <!-- 商品详情 -->
+      <div class="detail">
+        <h3>商品详情</h3>
+        <p>商品详情1</p>
+        <p>商品详情2</p>
+        <p>商品详情3</p>
+      </div>
       <!-- 商品详情 -->
       <!-- 分享 -->
       <!-- 分享 -->
@@ -108,32 +116,39 @@ export default {
 <style lang="less" scoped>
 .goodinfo-container {
   .goodinfo-main {
+    background: rgb(211, 124, 211);
     width: 1200px;
     margin: 0 auto;
     height: 1000px;
-    .left {
-      display: block;
-      float: left;
-      width: 510px;
-      .carousel {
-        width: 500px;
-        height: 500px;
-        margin: 10px;
-      }
-    }
-    .right {
-      float: left;
-      display: block;
-      width: 320px;
-      margin-left: 50px;
-      .price {
+    // display: flex;
+    .showgood {
+      height: 700px;
+      width: 100%;
+      border-bottom: solid 1px black;
+      .left {
         display: block;
-        font-size: 32px;
-        color: red;
-        // float: left;
+        float: left;
+        width: 510px;
+        .carousel {
+          width: 500px;
+          height: 500px;
+          margin: 10px;
+        }
       }
-      .col {
-        padding: 0;
+      .right {
+        float: left;
+        display: block;
+        width: 320px;
+        margin-left: 50px;
+        .price {
+          display: block;
+          font-size: 32px;
+          color: red;
+          // float: left;
+        }
+        .col {
+          padding: 0;
+        }
       }
     }
   }

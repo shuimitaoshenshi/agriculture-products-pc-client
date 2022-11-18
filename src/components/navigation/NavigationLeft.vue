66ecdb9fb9527a9393cfa850d24e6ee405aa6ceb
zxd <1281696826@qq.com>
2022-11-18 14:48:11 +0800

    nav_left

---
+++ src/components/navigation/NavigationLeft.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="navigationleft-container">
+    <v-navigation-drawer
+      class="deep-purple accent-4"
+      dark
+      absolute
+      style="top: 50px"
+    >
+      <v-list>
+        <v-list-item v-for="item in items" :key="item.title" link>
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+
+      <template v-slot:append>
+        <div class="pa-2">
+          <v-btn block> Logout </v-btn>
+        </div>
+      </template>
+    </v-navigation-drawer>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        { title: 'Dashboard', icon: 'mdi-view-dashboard' },
+        { title: 'Account', icon: 'mdi-account-box' },
+        { title: 'Admin', icon: 'mdi-gavel' }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.navigationleft-container {
+  display: block;
+  float: left;
+  aside {
+    top: 50px;
+  }
+}
+</style>
--- src/views/MyInfo/MyInfo.vue
+++ src/views/MyInfo/MyInfo.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="myinfo-container">
     <Head></Head>
+    <NavigationLeft></NavigationLeft>
   </div>
 </template>
 
 <script>
 import Head from '@/components/Head.vue'
+import NavigationLeft from '@/components/navigation/NavigationLeft.vue'
 export default {
   components: {
-    Head
+    Head,
+    NavigationLeft
   }
 }
 </script>

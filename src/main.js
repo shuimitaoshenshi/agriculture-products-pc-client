03a3a4a92e49dfdc405dcece722e197c2037ee87
zxd <1281696826@qq.com>
2023-01-07 11:06:13 +0800

    axios

--- src/main.js
+++ src/main.js
@@ -6,9 +6,11 @@ import $ from 'jquery'
 import '@/bootstrap/css/bootstrap.min.css'
 import '@/bootstrap/js/bootstrap.min.js'
 import '@/assets/css/base.css'
-
+import axios from 'axios'
+axios.defaults.baseURL = ''
 Vue.config.productionTip = false
 Vue.prototype.$ = $
+Vue.prototype.$http = axios
 new Vue({
   router,
   vuetify,
